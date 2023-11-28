e3b0e0b784f552c7d8b7180be12efe1d5e55cd94
abi-aleph <abi.farhan@aleph-labs.com>
2023-11-28 18:52:28 +0700

    Adding error scenario Internal Server Error

--- app/src/test/java/com/ourproject/register_module/datasource/http/usecase/RemoteRegisterFeedLoaderTest.kt
+++ app/src/test/java/com/ourproject/register_module/datasource/http/usecase/RemoteRegisterFeedLoaderTest.kt
@@ -175,7 +175,7 @@ class RemoteRegisterFeedLoaderTest{
             client.submitRegister(registerRequest)
         } returns flowOf(receivedResult)
 
-        val expectedResult = BadRequestException()
+        val expectedResult = BadRequest()
 
         sut.submit(userData = params).test {
             val currentResult = try {
@@ -219,6 +219,49 @@ class RemoteRegisterFeedLoaderTest{
         } returns flowOf(receivedResult)
 
         val expectedResult = NotFound()
+
+        sut.submit(userData = params).test {
+            val currentResult = try {
+                awaitItem()
+            } catch (e: Throwable) {
+                null
+            }
+
+            currentResult?.let {
+                when (it) {
+                    is RegisterFeedResult.Success -> {
+                        println("come to this condition 1 $it")
+                        // Handle success case if needed
+                    }
+
+                    is RegisterFeedResult.Failure -> {
+                        println("come to this condition 2 $it")
+                        assertEquals(expectedResult.message, it.throwable.message)
+                    }
+                }
+            }
+
+            awaitComplete()
+        }
+
+        // Verify that the submitRegister function was called
+        verify(exactly = 1) {
+            client.submitRegister(registerRequest)
+        }
+
+        // Confirm that there were no other interactions with the mock
+        confirmVerified(client)
+    }
+
+    @Test
+    fun testSubmitDeliversInternalServerError() = runBlocking {
+        val receivedResult = HttpClientResult.Failure(InternalServerErrorException())
+
+        every {
+            client.submitRegister(registerRequest)
+        } returns flowOf(receivedResult)
+
+        val expectedResult = InternalServerError()
 
         sut.submit(userData = params).test {
             val currentResult = try {
