2b6b578412a5f540a57793e9e6f1c72225170f67
abi-aleph <abi.farhan@aleph-labs.com>
2023-12-02 20:56:16 +0700

    Adding Login request not works

--- login_module/src/test/java/com/ourproject/login_module/feed/http/RemoteLoginFeedLoaderTest.kt
+++ login_module/src/test/java/com/ourproject/login_module/feed/http/RemoteLoginFeedLoaderTest.kt
@@ -1,12 +1,21 @@
 package com.ourproject.login_module.feed.http
 
+import app.cash.turbine.test
+import com.ourproject.login_module.feed.domain.LoginFeedResult
 import com.ourproject.login_module.feed.domain.LoginSubmitEntity
+import com.ourproject.register_module.datasource.http.usecase.RemoteRegisterFeedLoader
+import io.mockk.CapturingSlot
 import io.mockk.MockKAnnotations
 import io.mockk.clearAllMocks
 import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.slot
 import io.mockk.spyk
 import io.mockk.verify
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
@@ -44,7 +53,49 @@ class RemoteLoginFeedLoaderTest {
     @Test
     fun testSubmitDoesNotRequestLogin() {
         verify(exactly = 0) {
-            client.submitLogin()
+            client.submitLogin(loginRequest)
+        }
+
+        confirmVerified(client)
+    }
+
+    private fun expected(
+        sut: RemoteLoginFeedLoader,
+        receivedResult : HttpClientResult,
+        expectedResult: Any,
+        exactly: Int = -1,
+        slot: CapturingSlot<LoginSubmitDto> = slot<LoginSubmitDto>()
+    ) = runBlocking {
+        every {
+            client.submitLogin(capture(slot))
+        } returns flowOf(receivedResult)
+
+
+        sut.submit(params).test {
+            val currentResult = try {
+                awaitItem()
+            } catch (e: Throwable) {
+                null
+            }
+
+            currentResult?.let {
+                when(it){
+                    is LoginFeedResult.Success -> {
+                        Assert.assertEquals("birin2@gmail.com", slot.captured.email)
+                    }
+
+                    is LoginFeedResult.Failure -> {
+                        Assert.assertEquals(expectedResult::class.java, it.throwable::class.java)
+                    }
+                }
+            }
+
+            awaitComplete()
+        }
+
+
+        verify(exactly = exactly) {
+            client.submitLogin(loginRequest)
         }
 
         confirmVerified(client)
