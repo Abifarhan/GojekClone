23f7af4c78d44b4bd623410558076968b3f1eda8
abi-aleph <abi.farhan@aleph-labs.com>
2024-02-25 14:55:52 +0700

    Refactor RegisterRetrofitClient

--- feature/register/infrastructure/src/main/java/com/ourproject/infrastructure/remote/RegisterRetrofitClient.kt
+++ feature/register/infrastructure/src/main/java/com/ourproject/infrastructure/remote/RegisterRetrofitClient.kt
@@ -18,59 +18,55 @@ class RegisterRetrofitClient constructor(
     private val registerService: RegisterService
 ) : RegisterHttpClient {
     override fun register(body: RegisterSubmitRequest): Flow<HttpClientResult> {
-        return flow{
+        return flow {
             try {
-
-
-                val execution = registerService.register(
-                    registerBody = RemoteRegisterRequest(
-                        password = body.password,
-                        passwordConfirmation = body.password_confirmation,
-                        address = body.address,
-                        phoneNumber = body.phoneNumber,
-                        city = body.city,
-                        name = body.name,
-                        houseNumber = body.houseNumber,
-                        email = body.email
-                    )
-                )
-
-                val result = RegisterSubmitResponse(
-                    profilePhotoUrl = execution.remoteRegisterData.user.profilePhotoUrl,
-                    address = execution.remoteRegisterData.user.address,
-                    city = execution.remoteRegisterData.user.city,
-                    roles = execution.remoteRegisterData.user.roles,
-                    houseNumber = execution.remoteRegisterData.user.houseNumber,
-                    createdAt = execution.remoteRegisterData.user.createdAt,
-                    emailVerifiedAt = execution.remoteRegisterData.user.emailVerifiedAt,
-                    currentTeamId = execution.remoteRegisterData.user.currentTeamId,
-                    phoneNumber = execution.remoteRegisterData.user.phoneNumber,
-                    updatedAt = execution.remoteRegisterData.user.updatedAt,
-                    name = execution.remoteRegisterData.user.name,
-                    id = execution.remoteRegisterData.user.id,
-                    profilePhotoPath = execution.remoteRegisterData.user.profilePhotoPath,
-                    email = execution.remoteRegisterData.user.email
-                )
-                emit(HttpClientResult.Success(result))
+                val result = registerService.register(body.modelToDto()).remoteRegisterData
+                emit(HttpClientResult.Success(result.dtoToModel()))
             } catch (t: Throwable) {
-                when (t) {
-                    is IOException -> {
-                        emit(HttpClientResult.Failure(ConnectivityException()))
-                    }
+                val failureResult = when (t) {
+                    is IOException -> HttpClientResult.Failure(ConnectivityException())
                     is HttpException -> {
-                        when(t.code()){
-                            404 -> emit(HttpClientResult.Failure(NotFoundExceptionException()))
-                            422 -> emit(HttpClientResult.Failure(InvalidDataException()))
-                            500 -> emit(HttpClientResult.Failure(InternalServerErrorException()))
+                        when (t.code()) {
+                            404 -> HttpClientResult.Failure(NotFoundExceptionException())
+                            422 -> HttpClientResult.Failure(InvalidDataException())
+                            500 -> HttpClientResult.Failure(InternalServerErrorException())
+                            else -> HttpClientResult.Failure(UnexpectedException())
                         }
                     }
-                    else -> {
-                        emit(HttpClientResult.Failure(UnexpectedException()))
-                    }
+                    else -> HttpClientResult.Failure(UnexpectedException())
                 }
+                emit(failureResult)
             }
         }
     }
 
 
+    private fun RegisterSubmitRequest.modelToDto() = RemoteRegisterRequest(
+        password = this.password,
+        passwordConfirmation = this.password_confirmation,
+        address = this.address,
+        phoneNumber = this.phoneNumber,
+        city = this.city,
+        name = this.name,
+        houseNumber = this.houseNumber,
+        email = this.email
+    )
+
+
+    private fun RemoteRegisterResponse.RemoteRegisterDto.dtoToModel() = RegisterSubmitResponse(
+        profilePhotoUrl = this.user.profilePhotoUrl,
+        address = this.user.address,
+        city = this.user.city,
+        roles = this.user.roles,
+        houseNumber = this.user.houseNumber,
+        createdAt = this.user.createdAt,
+        emailVerifiedAt = this.user.emailVerifiedAt,
+        currentTeamId = this.user.currentTeamId,
+        phoneNumber = this.user.phoneNumber,
+        updatedAt = this.user.updatedAt,
+        name = this.user.name,
+        id = this.user.id,
+        profilePhotoPath = this.user.profilePhotoPath,
+        email = this.user.email
+    )
 }
